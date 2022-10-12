7df088f9e9fa94b861ce45e137bb063ca014d932
Yoochanhong <chanhongy6@naver.com>
2022-10-12 19:42:51 +0900

    :: 파츠 깃허브 링크 추가

--- lib/url.dart
+++ lib/url.dart
@@ -1,3 +1,4 @@
 final String gyuha = 'https://github.com/applely25';
 final String sanggu = 'https://github.com/MONOTYPEEE';
 final String huibeom = 'https://github.com/sinbla78';
+final String chanhong = 'https://github.com/Yoochanhong';
