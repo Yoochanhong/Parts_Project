5f382d83ef2773cb40dec6105e0772e68c72894e
Yoochanhong <chanhongy6@naver.com>
2022-10-12 21:56:57 +0900

    :: Stack으로 더 깔끔하게 배치

--- lib/main.dart
+++ lib/main.dart
@@ -30,77 +30,84 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Container(
         child: Center(
-          child: Column(
+          child: Stack(
+            clipBehavior: Clip.none,
             children: [
-              TextButton(
-                onPressed: () {
-                  launchURL(chanhong);
-                },
-                style: ButtonStyle(
-                  overlayColor: MaterialStateColor.resolveWith(
-                          (states) => Colors.transparent), //버튼 애니메이션 삭제
-                ),
-                child: Image(
-                  width: 300.0.w,
-                  height: 300.0.h,
-                  image: AssetImage('assets/head.png'),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      launchURL(huibeom);
-                    },
-                    child: Image(
-                      width: 200.0.w,
-                      height: 200.0.h,
-                      image: AssetImage('assets/leftarm.png'),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      launchURL(sanggu);
-                    },
-                    style: ButtonStyle(
-                      overlayColor: MaterialStateColor.resolveWith(
-                              (states) => Colors.transparent), //버튼 애니메이션 삭제
-                    ),
-                    child: Image(
-                      width: 200.0.w,
-                      height: 200.0.h,
-                      image: AssetImage('assets/rightarm.png'),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 150.0.h),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     TextButton(
                       onPressed: () {
-                        launchURL(gyuha);
+                        launchURL(chanhong);
                       },
                       style: ButtonStyle(
                         overlayColor: MaterialStateColor.resolveWith(
-                                (states) => Colors.transparent), //버튼 애니메이션 삭제
+                            (states) => Colors.transparent), //버튼 애니메이션 삭제
                       ),
                       child: Image(
-                        width: 200.0.w,
-                        height: 200.0.h,
-                        image: AssetImage('assets/leftleg.png'),
+                        width: 300.0.w,
+                        height: 300.0.h,
+                        image: AssetImage('assets/head.png'),
                       ),
-                    ),
-                    Image(
-                      width: 1.0.w,
-                      height: 1.0.h,
-                      image: AssetImage('assets/backgroung.png'),
                     ),
                   ],
                 ),
+              ),
+              Positioned(
+                left: 600.0.w,
+                top: 300.0.h,
+                child: TextButton(
+                  onPressed: () {
+                    launchURL(huibeom);
+                  },
+                  child: Image(
+                    width: 200.0.w,
+                    height: 200.0.h,
+                    image: AssetImage('assets/leftarm.png'),
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 600.0.w,
+                top: 300.0.h,
+                child: TextButton(
+                  onPressed: () {
+                    launchURL(sanggu);
+                  },
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.transparent), //버튼 애니메이션 삭제
+                  ),
+                  child: Image(
+                    width: 200.0.w,
+                    height: 200.0.h,
+                    image: AssetImage('assets/rightarm.png'),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 600.0.w,
+                bottom: 250.0.h,
+                child: TextButton(
+                  onPressed: () {
+                    launchURL(gyuha);
+                  },
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.transparent), //버튼 애니메이션 삭제
+                  ),
+                  child: Image(
+                    width: 200.0.w,
+                    height: 200.0.h,
+                    image: AssetImage('assets/leftleg.png'),
+                  ),
+                ),
+              ),
+              Image(
+                width: 1.0.w,
+                height: 1.0.h,
+                image: AssetImage('assets/backgroung.png'),
               ),
             ],
           ),
