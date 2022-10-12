c1e774f7b863b979cb03e0bd9b2d0b246bcb454a
Yoochanhong <chanhongy6@naver.com>
2022-10-12 19:19:48 +0900

    :: 화면에 이미지 넣기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:parts_project/link.dart';
+import 'package:parts_project/url.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,7 +28,56 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              Image(
+                width: 800.0.w,
+                height: 300.0.h,
+                image: AssetImage('assets/head.png'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(
+                    width: 1.0.w,
+                    height: 1.0.h,
+                    image: AssetImage('backgroung.png'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                    },
+                    child: Image(
+                      width: 500.0.w,
+                      height: 300.0.h,
+                      image: AssetImage('assets/rightarm.png'),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Image(
+                      width: 500.0.w,
+                      height: 300.0.h,
+                      image: AssetImage('assets/leftleg.png'),
+                    ),
+                  ),
+                  Image(
+                    width: 1.0.w,
+                    height: 1.0.h,
+                    image: AssetImage('assets/backgroung.png'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
