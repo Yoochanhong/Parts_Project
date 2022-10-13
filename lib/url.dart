50cba16d848f4d781d36275303acff6388aebf50
Yoochanhong <chanhongy6@naver.com>
2022-10-13 13:56:40 +0900

    :: 파츠 깃허브 링크 추가

--- lib/url.dart
+++ lib/url.dart
@@ -3,3 +3,4 @@ final String sanggu = 'https://github.com/MONOTYPEEE';
 final String huibeom = 'https://github.com/sinbla78';
 final String chanhong = 'https://github.com/Yoochanhong';
 final String sehyun = 'https://github.com/repairedserver';
+final String haeun = 'https://github.com/Gwonhaeun0908';
