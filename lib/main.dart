06c881dda4219b1489539618ea0b07d703cd09a3
Yoochanhong <chanhongy6@naver.com>
2022-10-14 14:54:18 +0900

    :: 전체적인 위젯 크기& 위치 수정

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyHomePage extends StatelessWidget {
         clipBehavior: Clip.none,
         children: [
           Positioned(
-            top: 180.0.h,
+            top: 100.0.h,
             left: 850.0.w,
             child: TextButton(
               onPressed: () {
@@ -54,7 +54,6 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: 90.0.h,
             left: 835.0.w,
             child: TextButton(
               onPressed: () {
@@ -75,7 +74,43 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Positioned(
-            left: 600.0.w,
+            left: 650.0.w,
+            bottom: 120.0.h,
+            child: TextButton(
+              onPressed: () {
+                launchURL(yeonu);
+              },
+              style: ButtonStyle(
+                overlayColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.transparent), //버튼 애니메이션 삭제
+              ),
+              child: Image(
+                width: 300.0.w,
+                height: 400.0.h,
+                image: AssetImage('assets/messiLeftLeg.png'),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 805.0.w,
+            top: 300.0.h,
+            child: TextButton(
+              onPressed: () {
+                launchURL(sehyun);
+              },
+              style: ButtonStyle(
+                overlayColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.transparent), //버튼 애니메이션 삭제
+              ),
+              child: Image(
+                width: 300.0.w,
+                height: 250.0.h,
+                image: AssetImage('assets/body.png'),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 599.0.w,
             top: 300.0.h,
             child: TextButton(
               onPressed: () {
@@ -83,12 +118,33 @@ class MyHomePage extends StatelessWidget {
               },
               style: ButtonStyle(
                 overlayColor: MaterialStateColor.resolveWith(
-                    (states) => Colors.transparent), //버튼 애니메이션 삭제
+                        (states) => Colors.transparent), //버튼 애니메이션 삭제
               ),
               child: Image(
-                width: 200.0.w,
-                height: 200.0.h,
+                width: 250.0.w,
+                height: 250.0.h,
                 image: AssetImage('assets/leftarm.png'),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 560.0.w,
+            top: 378.0.h,
+            child: TextButton(
+              onPressed: () {
+                launchURL(haeun);
+              },
+              style: ButtonStyle(
+                overlayColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.transparent), //버튼 애니메이션 삭제
+              ),
+              child: Transform.rotate(
+                angle: magicWandRadians,
+                child: Image(
+                  width: 180.0.w,
+                  height: 180.0.h,
+                  image: AssetImage('assets/magicWand.png'),
+                ),
               ),
             ),
           ),
@@ -101,69 +157,12 @@ class MyHomePage extends StatelessWidget {
               },
               style: ButtonStyle(
                 overlayColor: MaterialStateColor.resolveWith(
-                    (states) => Colors.transparent), //버튼 애니메이션 삭제
+                        (states) => Colors.transparent), //버튼 애니메이션 삭제
               ),
               child: Image(
-                width: 200.0.w,
-                height: 200.0.h,
+                width: 250.0.w,
+                height: 250.0.h,
                 image: AssetImage('assets/rightarm.png'),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 550.0.w,
-            top: 345.0.h,
-            child: TextButton(
-              onPressed: () {
-                launchURL(haeun);
-              },
-              style: ButtonStyle(
-                overlayColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.transparent), //버튼 애니메이션 삭제
-              ),
-              child: Transform.rotate(
-                angle: magicWandRadians,
-                child: Image(
-                  width: 150.0.w,
-                  height: 150.0.h,
-                  image: AssetImage('assets/magicWand.png'),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 600.0.w,
-            bottom: 180.0.h,
-            child: TextButton(
-              onPressed: () {
-                launchURL(yeonu);
-              },
-              style: ButtonStyle(
-                overlayColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.transparent), //버튼 애니메이션 삭제
-              ),
-              child: Image(
-                width: 270.0.w,
-                height: 300.0.h,
-                image: AssetImage('assets/messiLeftLeg.png'),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 600.0.w,
-            top: 300.0.h,
-            child: TextButton(
-              onPressed: () {
-                launchURL(sehyun);
-              },
-              style: ButtonStyle(
-                overlayColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.transparent), //버튼 애니메이션 삭제
-              ),
-              child: Image(
-                width: 300.0.w,
-                height: 300.0.h,
-                image: AssetImage('assets/body.png'),
               ),
             ),
           ),
